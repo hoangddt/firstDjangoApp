a068fd10d49cb7f38d749ea2d1a913be9923230a
Quoc Hoang <quochoangddt@gmail.com>
2015-01-31 15:43:15 +0700

    Add __str__ method to Question and Choice

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,9 +3,17 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    # make sure it display the question_text instead of Question object
+    def __str__(self):
+    	# Change name __str__ by __unicode__ on Python 2
+    	return self.question_text
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    # make sure it display the choice_text instead of Question object
+    def __str__(self):
+    	# Change name __str__ by __unicode__ on Python 2
+    	return self.choice_text
