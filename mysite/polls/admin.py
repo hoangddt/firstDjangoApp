5ddad9587b64510a43f2f48b316d1db561520f05
Quoc Hoang <quochoangddt@gmail.com>
2015-01-31 21:13:01 +0700

    Step#2 Add filter by day functionality to Question page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -10,6 +10,8 @@ class QuestionAdmin(admin.ModelAdmin):
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes':['collapse']}),
     ]
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date']
     inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
