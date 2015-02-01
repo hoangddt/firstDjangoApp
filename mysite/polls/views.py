f6afdeea88ba625441decda430e04f828957eef6
Quoc Hoang <quochoangddt@gmail.com>
2015-02-01 23:03:25 +0700

    Step#4 dealing with POST data in vote() function

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from polls.models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from polls.models import Question, Choice
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -16,4 +17,18 @@ def results(request, question_id):
 	return HttpResponse(response%question_id)
 
 def vote(request, question_id):
-	return HttpResponse("You are voting on question: %s" %question_id)
+	p = get_object_or_404(Question, pk = question_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(request, 'polls/detail.html',{
+				'question':p,
+				'error_message':"You didn't select a choice",
+			})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		# Always return an HttpResponseRedirect after successfully dealing
+		# with POST data. This prevents data from being posted twice if a
+		# user hits the Back button.
+		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
